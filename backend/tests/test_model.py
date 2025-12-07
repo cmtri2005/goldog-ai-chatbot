d60cb45e40cdf42ae3224b683693a316bf075950
cmtri2005 <tricaominh2005@gmail.com>
2025-12-07 14:40:09 +0000

    refactor project structure

--- backend/tests/test_model.py
+++
@@ -1,8 +0,0 @@
-from langchain_aws import ChatBedrockConverse
-from langchain_core.messages import HumanMessage
-
-model_id = "meta.llama3-70b-instruct-v1:0"  
-region = "us-east-1" 
-
-llm = ChatBedrockConverse(model=model_id, region_name=region)
-print(llm.invoke([HumanMessage(content="Tôi muốn mua nhà ở chợ Bình Chánh")]).content)
